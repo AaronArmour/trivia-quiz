c4d641e4d6719824819f1087cb2daa6913421f58
Aaron Armour <aaronarmour83@gmail.com>
2024-09-24 16:31:19 +1200

    Tidy: Remove commented out code from cli-client

--- apps/cli-client/src/index.ts
+++ apps/cli-client/src/index.ts
@@ -12,27 +12,7 @@ socket.on(
   async (message: any) => await messageHandler(socket, message),
 );
 
-// socket.on('new-game', async (message) => await messageHandler(socket, message));
-// socket.on('question', async (message) => await messageHandler(socket, message));
-// socket.on('grading', async (message) => await messageHandler(socket, message));
-// socket.on('score', async (message) => await messageHandler(socket, message));
-// socket.on('text', async (message) => await messageHandler(socket, message));
-
 // Error handling
 socket.on('connect_error', (error) => {
   console.error('Socket.IO Connection Error: ', error);
 });
-
-// import { WebSocket } from 'ws';
-// import { messageHandler } from './messageHandler';
-
-// const PORT = Number(process.env.PORT) || 8081;
-// const HOST = process.env.HOST || 'localhost';
-
-// const ws = new WebSocket(`ws://${HOST}:${PORT}`);
-
-// ws.on('message', async (message) => await messageHandler(ws, message));
-
-// ws.onerror = function (error) {
-//   console.error('WebSocket Error: ', error);
-// };
--- apps/cli-client/src/messageHandler.ts
+++ apps/cli-client/src/messageHandler.ts
@@ -3,10 +3,7 @@ import kleur from 'kleur';
 
 import { getUserAnswer, getYesNoResponse } from './userInput';
 
-export async function messageHandler(
-  socket: Socket,
-  message: any, // The type depends on what your server sends
-) {
+export async function messageHandler(socket: Socket, message: any) {
   switch (message.mType) {
     case 'text':
       console.log(message.text);
@@ -53,61 +50,3 @@ export async function messageHandler(
       console.log(`Unrecognized message type. Received response: ${message}`);
   }
 }
-
-// import WebSocket from 'ws';
-// import kleur from 'kleur';
-
-// import { getUserAnswer, getYesNoResponse } from './userInput';
-
-// export async function messageHandler(
-//   ws: WebSocket,
-//   message: WebSocket.RawData,
-// ) {
-//   const mObj = JSON.parse(message.toString('utf-8'));
-
-//   switch (mObj.mType) {
-//     case 'text':
-//       console.log(mObj.text);
-//       break;
-//     case 'question':
-//       const userAnswer = await getUserAnswer(mObj);
-
-//       ws.send(
-//         JSON.stringify({
-//           mType: 'answer',
-//           id: mObj.id,
-//           answer: userAnswer,
-//         }),
-//       );
-//       break;
-//     case 'grading':
-//       if (mObj.correct) {
-//         console.log(kleur.green('Correct!'));
-//       } else {
-//         console.log(kleur.red('Incorrect!'));
-//         console.log(kleur.red(`Correct answer: ${mObj.correctAnswer}`));
-//       }
-//       console.log();
-//       break;
-//     case 'score':
-//       // End of the quiz, display the final score and prompt to play again
-//       console.log(`You got ${mObj.correct} out of ${mObj.total} correct!`);
-//       if (
-//         (await getYesNoResponse('Do you want to play another quiz?')) === 'y'
-//       ) {
-//         ws.send(JSON.stringify({ mType: 'start' }));
-//       } else {
-//         ws.close();
-//       }
-//       break;
-//     case 'new-game':
-//       if ((await getYesNoResponse(mObj.text)) === 'y') {
-//         ws.send(JSON.stringify({ mType: 'start' }));
-//       } else {
-//         ws.close();
-//       }
-//       break;
-//     default:
-//       console.log(`Unrecognised message type. Received response: ${message}`);
-//   }
-// }
