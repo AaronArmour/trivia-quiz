c3dba30e1f16a9395a4a6313bd51779e7359bdc6
Aaron Armour <aaronarmour83@gmail.com>
2024-10-07 14:04:45 +1300

    Add getCircularReplacer function to protect JSON.stringify from circular refs in objects

--- apps/server/src/logger.ts
+++ apps/server/src/logger.ts
@@ -3,12 +3,27 @@ import { createLogger, format, transports } from 'winston';
 const { combine, timestamp, printf, colorize } = format;
 const LOG_LEVEL = process.env.LOG_LEVEL || 'info';
 
+function getCircularReplacer() {
+  const seen = new WeakSet<object>();
+  return (key: string, value: any) => {
+    if (typeof value === 'object' && value !== null) {
+      if (seen.has(value)) {
+        return '[Circular]';
+      }
+      seen.add(value);
+    }
+    return value;
+  };
+}
+
 // Define a custom format for the log output with colors and labels
 const customFormat = printf(({ level, message, timestamp, ...meta }) => {
   const { playerId } = meta;
   delete meta.playerId;
   const playerIdString = playerId ? `(${playerId}) ` : '';
-  const metaString = Object.keys(meta).length ? ` ${JSON.stringify(meta)}` : '';
+  const metaString = Object.keys(meta).length
+    ? ` ${JSON.stringify(meta, getCircularReplacer())}`
+    : '';
 
   return `${timestamp} ${playerIdString}[${level}]: ${message}${metaString}`;
 });
