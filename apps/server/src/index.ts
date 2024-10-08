daee13ec24e3dc1b865cfd44b4803eaef8336843
Aaron Armour <aaronarmour83@gmail.com>
2024-10-08 14:47:41 +1300

    Rework of server, cache messages for possible resending

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -2,10 +2,9 @@ import { createServer } from 'http';
 import { Server } from 'socket.io';
 
 import { NUM_QNS } from './constants';
-import { generateUniqueId } from '@quiz-lib/utils';
 import { messageHandler } from './messageHandler';
 import { log } from './logger';
-import { ServerMessageType } from '@quiz-lib/core';
+import { ServerMessage, ServerMessageType } from '@quiz-lib/core';
 
 log('info', `Configured with NUM_QNS=${NUM_QNS}`);
 
@@ -18,13 +17,59 @@ const io = new Server(httpServer);
 const players: { [k: string]: any } = {};
 
 io.on('connection', (socket) => {
-  const playerId = generateUniqueId('player');
-  const player = {
-    id: playerId,
-  };
+  const playerId = socket.handshake.auth.playerId;
+  let player;
 
-  players[playerId] = player;
-  log('info', 'Player connected', playerId);
+  log('info', 'Players object', undefined, players);
+  if (players[playerId]) {
+    log('info', 'Player reconnected', playerId);
+    player = players[playerId];
+    player.socket = socket;
+
+    const clientOffset = socket.handshake.auth.msgOffset;
+    if (clientOffset < player.msgCache.length) {
+      log(
+        'info',
+        `Delivering ${player.msgCache.length - clientOffset} cached message(s)`,
+        playerId,
+      );
+
+      player.msgCache
+        .slice(clientOffset)
+        .forEach((message: ServerMessage, index: number) => {
+          log('debug', 'Resending cached message:', playerId, message);
+          socket.emit('message', message, clientOffset + index + 1);
+        });
+    }
+  } else {
+    log('info', 'Player connected', playerId);
+    player = {
+      id: playerId,
+      msgOffset: 0,
+      msgCache: [],
+      socket,
+      emit(message: ServerMessage) {
+        this.socket.emit('message', message, ++this.msgOffset);
+        this.msgCache.push(message);
+      },
+      toJSON() {
+        return {
+          type: 'Player',
+          id: this.id,
+          ...(this.quiz && { quiz: this.quiz.toJSON() }),
+        };
+      },
+    };
+    players[playerId] = player;
+
+    player.emit({
+      type: ServerMessageType.NEW_GAME,
+      payload: {
+        text: 'Do you want to play a quiz?',
+      },
+    });
+  }
+  log('info', 'Players object is now', undefined, players);
 
   socket.onAny((event, ...args) => {
     log('debug', `Received socket '${event}' event:`, playerId, args);
@@ -35,19 +80,11 @@ io.on('connection', (socket) => {
   });
 
   socket.on('message', async (message) => {
-    await messageHandler(socket, player, message);
+    await messageHandler(player, message);
   });
 
   socket.on('disconnect', () => {
     log('info', 'Player disconnected', playerId);
-    delete players[playerId];
-  });
-
-  socket.emit('message', {
-    type: ServerMessageType.NEW_GAME,
-    payload: {
-      text: 'Do you want to play a quiz?',
-    },
   });
 });
 
